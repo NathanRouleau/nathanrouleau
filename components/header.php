6ad3f077cc32dedec061514f85c32f89735feb00
Nathan ROULEAU <nathan@dotnet-sas.net>
2023-10-26 16:11:38 +0200

    Commit vers Git

---
+++ components/header.php
@@ -0,0 +1,6 @@
+<nav class="thx">
+        <a href="/index.html">Home</a>
+        <a href="/about/about.html">About</a>
+        <a href="projets/projets.html">Projects</a>
+        <a href="contact/contact.html">Contact</a>
+</nav>
